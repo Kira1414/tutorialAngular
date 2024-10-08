04fd999059ad9ea63f1f69077a5f2f3932b31ac3
Kira1414 <aspenag@libertadores.edu.co>
2024-10-07 19:45:05 -0500

    first angular concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tutorialAngular';
+  city = 'San Francisco';
+  isLoggedIn = true;
+  isServerRunning = true;
+  operatingSystems = [{id: 'win', name: 'Windows'}, {id: 'osx', name: 'MacOS'}, {id: 'linux', name: 'Linux'}];
+  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
+  isEditable = true;
+  message = "There's a secret message for you, hover to reveal 👀";
+
+  onMouseOver(){
+    this.message = 'Way to go 🚀'
+  }
 }
